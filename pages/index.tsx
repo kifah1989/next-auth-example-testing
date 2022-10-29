3f64aa62ff42f57f60e773abad5bd30b418ae9b5
kifah1989 <kifah.andary@gmail.com>
2022-10-29 14:30:00 +0300

    clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,13 @@
-import { getProviders, signIn, useSession } from 'next-auth/react';
-import Head from 'next/head'
-import Image from 'next/image'
+import { useSession } from 'next-auth/react';
 import Layout from '../components/layout';
-import styles from '../styles/Home.module.css'
 
-export default function Home({providers}:any) {
-    const { data } = useSession();
+export default function Home({ name }: any) {
+  const { data } = useSession();
 
   return (
     <Layout>
       <pre>{JSON.stringify(data, null, 2)}</pre>
-      <div>
-        {Object.values(providers).map((provider:any) => {
-          return (
-            <div key={provider.name}>
-              <button onClick={() => signIn(provider.id)}>
-                Sign in with {provider.name}
-              </button>
-            </div>
-          );
-        })}
-      </div>
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      providers: await getProviders(),
-    },
-  };
-}
